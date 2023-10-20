41543deda2b71b504c77d101af6aafe9dfe1e15c
Laureano73 <laureanotrilo@hotmail.com>
2023-10-20 13:44:31 -0300

    final

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,5 @@
 import ItemList from '../ItemList/ItemList'
 import React, { useState, useEffect, useContext } from 'react';
-import productos from '../../inventario.json';
 import { useParams } from 'react-router-dom'
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../firebase/config"
@@ -12,42 +11,23 @@ function ItemListContainer() {
     // const context = useContext(ThemeContext)
     // console.log(context)
 
-    const getData = () => {
-        return new Promise((resolve) => {
-            categoryId ?
-                resolve(productos.filter((producto) => producto.category === categoryId))
-                : resolve(productos);
-        });
-    }
 
     useEffect(() => {
 
         //configuramos la referencia de nuestros productos
         const productsRef = collection(db, "productos")
         //Utilizamos la funcion getDocs para obtener todos los productos
-        getDocs(productsRef).then((response) => {
 
-            //Formateamos la data a un array de objetos
-            const productsFirebase = response.docs.map((product) => (
-                { id: product.id, ...product.data() }
-            ))
-            console.log(productsFirebase)
-        })
-
-        const q = query(productsRef, where("category", "==", "Papas"))
+        const q = query(productsRef, categoryId === undefined ? undefined:  where("category", "==", categoryId))
         getDocs(q).then((response)=> {
             const productsFirebase = response.docs.map((product)=>(
-                {id: product.id, ...product.data()}
+                {id: product.data().title, ...product.data()}
             ))
             console.log(productsFirebase)
+            setProductos (productsFirebase)
         })
 
-
-        getData()
-            .then((res) => {
-                setProductos(res);
-            });
-    }, [categoryId, productos]);
+    }, [categoryId]);
 
     return (
         <>
